5040235a0e27dbf849fdf4c45167c9dfbe91ba35
Jhorman Tito <jhormanrus@gmail.com>
2023-07-23 11:23:48 -0500

    button outline

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,4 +1,4 @@
-<button class="border-2 border-gray-300 rounded-lg bg-white px-4 py-2 font-semibold text-stone-800 inline-flex justify-center items-center gap-2 transition-shadow">
+<button class="border-2 border-gray-300 rounded-lg bg-white px-4 py-2 font-medium text-stone-800 inline-flex justify-center items-center gap-2 transition-shadow text-base leading-none h-10">
   <slot />
 </button>
 
@@ -9,5 +9,9 @@
   }
   button:where(:not(:active):hover) {
     --highlight-size: .375rem;
+  }
+  button:focus-visible {
+    outline-color: blue;
+    outline-offset: 5px;
   }
 </style>
