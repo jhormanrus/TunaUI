1e47262b881b391e407d924c3532bbaa72041803
Jhorman <jhormanrus@gmail.com>
2023-09-17 22:01:01 +0000

    ✨ migrate tailwind to mastercss

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,14 +1,18 @@
 <button
-  class="border-2 border-gray-300 rounded-lg bg-white px-4 py-2 font-medium text-stone-800 inline-flex justify-center items-center gap-2 transition-shadow text-base leading-none h-10"
+  class="b:2 b:gray-80 r:8 bg:white px:16 py:8 font:medium fg:gray-20 inline-flex jc:center ai:center gap:8 f:16 h:40"
   type="button"
 >
   <slot />
 </button>
 
-<style lang="postcss">
+<style>
   button {
     --highlight-size: 0;
     box-shadow: 0 7px 14px -5px hsl(220 3% 15%/calc(1% + 5%)), 0 1px #dee2e6,0 0 0 var(--highlight-size) hsl(210 11% 71%/25%);
+
+    transition-property: box-shadow;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 150ms;
   }
   button:where(:not(:active):hover) {
     --highlight-size: .375rem;
