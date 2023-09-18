f310e6968b20d50a1eb4f89bec3a1dad99dc3e9f
Jhorman <jhormanrus@gmail.com>
2023-09-18 23:57:11 +0000

    ✨ migrate tailwind to mastercss

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,86 +1,33 @@
 <script lang="ts">
-  export let id: string
+  export let id: string | null = null
   export let type: 'text' | 'password' | 'email' | 'number' | 'tel' | 'url' | 'search' | 'date' | 'time' | 'datetime-local' | 'month' | 'week' = 'text'
   export let size: 'sm' | 'lg' | null = null
   export let label: string
   export let placeholder = ''
 </script>
 
-<div class="input-field" class:sm={size === 'sm'} class:lg={size === 'lg'}>
+<div class="rel">
+  <span
+    class="abs fg:gray-50 top:0 left:0 pointer-events:none transform:top|left translate(12,16) scale(1) fg:gray-50 fg:blue:has(+:focus) translateY(8):has(+:focus) scale(.75):has(+:focus) {size === 'sm' && 'translateY(12)! scale(1)! translateY(4)!:has(+:focus) scale(.75)!:has(+:focus)'} {size === 'lg' && 'f:18! translateY(20) scale(1) translateY(12)!:has(+:focus) scale(.75)!:has(+:focus)'}"
+  >{label}</span>
   <input
+    class="w:full bg:gray-80 outline:blue r:8 px:12 pt:24 pb:8 fg:transparent::placeholder fg:gray-60:focus::placeholder fg:gray-60:valid::placeholder fg:gray-90:hover fg:gray-80:focus {size === 'sm' && 'pt:20! pb:4!'} {size === 'lg' && 'f:18! px:16! pt:32! pb:12!'}"
     id={id}
     type={type}
     placeholder={placeholder}
     required
   />
-  <label for={id}>{label}</label>
 </div>
 
-<style lang="postcss">
-  .input-field {
-    @apply relative;
+<style>
+  input {
+    transition-property: color, background-color, border-color, text-decoration-color, fill, stroke;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 150ms;
   }
-  .input-field > input {
-    @apply w-full;
-    @apply bg-gray-200;
-    @apply outline-blue-500;
-    @apply rounded-lg;
-    @apply px-3 pt-6 pb-2;
-    @apply transition-colors;
-  }
-  .input-field > input::placeholder {
-    @apply text-transparent;
-    @apply transition-colors;
-  }
-  .input-field > input:focus::placeholder,
-  .input-field > input:valid::placeholder {
-    @apply text-gray-400;
-  }
-  .input-field > input:hover {
-    @apply bg-gray-100;
-  }
-  .input-field > input:focus {
-    @apply bg-gray-200;
-  }
-
-  .input-field > label {
-    @apply absolute;
-    @apply text-gray-500;
-    @apply top-0 left-0;
-    @apply transition-transform;
-    @apply pointer-events-none;
-    @apply origin-top-left;
-    @apply translate-x-3 translate-y-4 scale-100;
-  }
-  .input-field > input:focus ~ label {
-    @apply text-blue-700;
-    @apply translate-y-2 scale-75;
-  }
-  .input-field > input:valid ~ label {
-    @apply translate-y-2 scale-75;
-  }
-
-  .input-field.sm > input {
-    @apply pt-5 pb-1;
-  }
-  .input-field.sm > label {
-    @apply translate-y-3 scale-100;
-  }
-  .input-field.sm > input:focus ~ label,
-  .input-field.sm > input:valid ~ label {
-    @apply translate-y-1 scale-75;
-  }
-
-  .input-field.lg > input {
-    @apply text-lg;
-    @apply px-4 pt-8 pb-3;
-  }
-  .input-field.lg > label {
-    @apply text-lg;
-    @apply translate-x-4 translate-y-5 scale-100;
-  }
-  .input-field.lg > input:focus ~ label,
-  .input-field.lg > input:valid ~ label {
-    @apply translate-y-3 scale-75;
+  span {
+    transition-property: transform;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 150ms;
   }
 </style>
---
+++ src/stories/Input.story.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { Input } from '$lib/index.js'
+  import type { Hst } from '@histoire/plugin-svelte'
+
+  export let Hst: Hst
+</script>
+
+<Hst.Story
+  title="Input"
+  layout={{ type: 'grid', width: 240 }}
+>
+
+  <Hst.Variant title="primary">
+    <Input label="Input" />
+  </Hst.Variant>
+
+  <Hst.Variant title="secondary">
+    <Input label="Input" size="sm" />
+  </Hst.Variant>
+
+  <Hst.Variant title="destructive">
+    <Input label="Input" size="lg" />
+  </Hst.Variant>
+
+  <Hst.Variant title="outline">
+  </Hst.Variant>
+
+  <Hst.Variant title="ghost">
+  </Hst.Variant>
+
+  <Hst.Variant title="link">
+  </Hst.Variant>
+
+  <Hst.Variant title="icon">
+  </Hst.Variant>
+
+  <Hst.Variant title="with icon">
+  </Hst.Variant>
+
+  <Hst.Variant title="loading">
+  </Hst.Variant>
+
+  <Hst.Variant title="as child">
+  </Hst.Variant>
+
+</Hst.Story>
