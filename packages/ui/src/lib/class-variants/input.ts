18c679330465af75fbed9cc9847da379210d622a
Jhorman <jhormanrus@gmail.com>
2024-01-21 22:15:31 +0000

    ✨ refactor input design

--- packages/ui/src/lib/class-variants/input.ts
+++ packages/ui/src/lib/class-variants/input.ts
@@ -4,15 +4,21 @@ export const label = cv(
   'abs fg:gray-50 fg:control-label:has(+:focus) left:0 pointer-events:none top:0 transform-origin:top|left transition:transform|100ms|ease-in',
   {
     size: {
-      sm: 'f:16 translate(12,14) translate(12,8)scale(.75):has(+:not(:placeholder-shown),+:focus)',
-      md: 'f:16 translate(14,18) translate(14,10)scale(.75):has(+:not(:placeholder-shown),+:focus)',
-      lg: 'f:18 translate(16,22) translate(16,14)scale(.75):has(+:not(:placeholder-shown),+:focus)',
+      sm: 'translate(12,14) translate(12,8)scale(.75):has(+:not(:placeholder-shown),+:focus)',
+      md: 'translate(14,18) translate(14,10)scale(.75):has(+:not(:placeholder-shown),+:focus)',
+      lg: 'translate(16,22) translate(16,14)scale(.75):has(+:not(:placeholder-shown),+:focus)',
     },
   },
 )
 
 export const wrapper = cv(
-  'block rel bg:control-bg bg:control-hover-bg:hover bg:control-hover-bg:focus outline:control-outline|solid|2:has(input:focus-visible) transition:background-color|150ms|ease-in',
+  `
+    block rel
+    bg:gray-5 bg:gray-10:has(:not(input:focus-visible):hover)
+    transition:background-color|150ms|ease-in,box-shadow|150ms|ease-in
+    $highlight-size:0 $highlight-size:1px:has(input:focus-visible)
+    shadow:0|0|0|$(highlight-size)|gray-50,0|0|0|calc($(highlight-size)*5)|gray-50/.2
+  `,
   {
     size: {
       sm: 'f:16 pb:5 pt:21 px:12 r:10',
