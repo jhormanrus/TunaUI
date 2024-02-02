82c65b65d160882d5925f7bdf3b0819332b64120
Jhorman <jhormanrus@gmail.com>
2024-02-02 23:15:01 +0000

    ✨ more icons

--- packages/ui/src/stories/icon/Icon.story.vue
+++ packages/ui/src/stories/icon/Icon.story.vue
@@ -1,54 +1,66 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import IconX from '../../components/icon/IconX.vue'
-import IconSelector from '../../components/icon/IconSelector.vue'
-import IconSearch from '../../components/icon/IconSearch.vue'
-import IconsCoins from '../../components/icon/IconsCoins.vue'
-import IconMoodHappy from '../../components/icon/IconMoodHappy.vue'
-import IconInfoCircle from '../../components/icon/IconInfoCircle.vue'
-import IconCreditCard from '../../components/icon/IconCreditCard.vue'
-import IconClock from '../../components/icon/IconClock.vue'
-import IconClipboard from '../../components/icon/IconClipboard.vue'
-import IconChevronUp from '../../components/icon/IconChevronUp.vue'
-import IconChevronRight from '../../components/icon/IconChevronRight.vue'
-import IconChevronLeft from '../../components/icon/IconChevronLeft.vue'
-import IconChevronDown from '../../components/icon/IconChevronDown.vue'
-import IconChecks from '../../components/icon/IconChecks.vue'
-import IconCheck from '../../components/icon/IconCheck.vue'
-import IconChartBar from '../../components/icon/IconChartBar.vue'
-import IconCaretDown from '../../components/icon/IconCaretDown.vue'
-import IconCalendar from '../../components/icon/IconCalendar.vue'
-import IconBoxMultiple from '../../components/icon/IconBoxMultiple.vue'
-import IconBookmarks from '../../components/icon/IconBookmarks.vue'
-import IconBookmark from '../../components/icon/IconBookmark.vue'
-import IconBellRinging from '../../components/icon/IconBellRinging.vue'
-import IconBell from '../../components/icon/IconBell.vue'
-import IconBackspace from '../../components/icon/IconBackspace.vue'
-import IconAt from '../../components/icon/IconAt.vue'
-import IconAssembly from '../../components/icon/IconAssembly.vue'
-import IconArchive from '../../components/icon/IconArchive.vue'
-import IconAlertCircle from '../../components/icon/IconAlertCircle.vue'
-import IconAdjustmentsHorizontal from '../../components/icon/IconAdjustmentsHorizontal.vue'
 import IconAdjustments from '../../components/icon/IconAdjustments.vue'
+import IconAdjustmentsHorizontal from '../../components/icon/IconAdjustmentsHorizontal.vue'
+import IconAlertCircle from '../../components/icon/IconAlertCircle.vue'
+import IconArchive from '../../components/icon/IconArchive.vue'
+import IconAssembly from '../../components/icon/IconAssembly.vue'
+import IconAt from '../../components/icon/IconAt.vue'
+import IconBackspace from '../../components/icon/IconBackspace.vue'
+import IconBell from '../../components/icon/IconBell.vue'
+import IconBellRinging from '../../components/icon/IconBellRinging.vue'
+import IconBookmark from '../../components/icon/IconBookmark.vue'
+import IconBookmarks from '../../components/icon/IconBookmarks.vue'
+import IconBoxMultiple from '../../components/icon/IconBoxMultiple.vue'
+import IconCalendar from '../../components/icon/IconCalendar.vue'
+import IconCaretDown from '../../components/icon/IconCaretDown.vue'
+import IconChartBar from '../../components/icon/IconChartBar.vue'
+import IconCheck from '../../components/icon/IconCheck.vue'
+import IconChecks from '../../components/icon/IconChecks.vue'
+import IconChevronDown from '../../components/icon/IconChevronDown.vue'
+import IconChevronLeft from '../../components/icon/IconChevronLeft.vue'
+import IconChevronRight from '../../components/icon/IconChevronRight.vue'
+import IconChevronUp from '../../components/icon/IconChevronUp.vue'
+import IconClipboard from '../../components/icon/IconClipboard.vue'
+import IconClock from '../../components/icon/IconClock.vue'
+import IconColorSwatch from '../../components/icon/IconColorSwatch.vue'
+import IconCookie from '../../components/icon/IconCookie.vue'
+import IconCreditCard from '../../components/icon/IconCreditCard.vue'
+import IconCrop from '../../components/icon/IconCrop.vue'
+import IconCut from '../../components/icon/IconCut.vue'
+import IconDots from '../../components/icon/IconDots.vue'
+import IconDotsVertical from '../../components/icon/IconDotsVertical.vue'
+import IconEye from '../../components/icon/IconEye.vue'
+import IconEyeClosed from '../../components/icon/IconEyeClosed.vue'
+import IconFile from '../../components/icon/IconFile.vue'
+import IconFiles from '../../components/icon/IconFiles.vue'
+import IconInfoCircle from '../../components/icon/IconInfoCircle.vue'
+import IconMoodHappy from '../../components/icon/IconMoodHappy.vue'
+import IconsCoins from '../../components/icon/IconsCoins.vue'
+import IconSearch from '../../components/icon/IconSearch.vue'
+import IconSelector from '../../components/icon/IconSelector.vue'
+import IconX from '../../components/icon/IconX.vue'
 
 const width = ref()
 const height = ref()
 
 const icons = [
-  { name: 'adjustments', component: IconAdjustments },
   { name: 'adjustments-horizontal', component: IconAdjustmentsHorizontal },
+  { name: 'adjustments', component: IconAdjustments },
   { name: 'alert-circle', component: IconAlertCircle },
   { name: 'archive', component: IconArchive },
   { name: 'assembly', component: IconAssembly },
   { name: 'at', component: IconAt },
   { name: 'backspace', component: IconBackspace },
-  { name: 'bell', component: IconBell },
   { name: 'bell-ringing', component: IconBellRinging },
+  { name: 'bell', component: IconBell },
   { name: 'bookmark', component: IconBookmark },
   { name: 'bookmarks', component: IconBookmarks },
   { name: 'box-multiple', component: IconBoxMultiple },
+  { name: 'calendar', component: IconCalendar },
   { name: 'caret-down', component: IconCaretDown },
   { name: 'chart-bar', component: IconChartBar },
+  { name: 'check', component: IconCheck },
   { name: 'checks', component: IconChecks },
   { name: 'chevron-down', component: IconChevronDown },
   { name: 'chevron-left', component: IconChevronLeft },
@@ -56,10 +68,8 @@ const icons = [
   { name: 'chevron-up', component: IconChevronUp },
   { name: 'clipboard', component: IconClipboard },
   { name: 'clock', component: IconClock },
-  { name: 'calendar', component: IconCalendar },
-  { name: 'check', component: IconCheck },
-  { name: 'credit-card', component: IconCreditCard },
   { name: 'coins', component: IconsCoins },
+  { name: 'credit-card', component: IconCreditCard },
   { name: 'info-circle', component: IconInfoCircle },
   { name: 'mood-happy', component: IconMoodHappy },
   { name: 'search', component: IconSearch },
