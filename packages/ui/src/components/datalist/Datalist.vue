839a09ab4c217a40fa53578dc6366a51c061768f
Jhorman <jhormanrus@gmail.com>
2024-02-14 03:09:51 +0000

    🚀 refactor datalist and select components

--- packages/ui/src/components/datalist/Datalist.vue
+++ packages/ui/src/components/datalist/Datalist.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg:white b:gray-20 b:1 r:12 px:14 py:8 shadow">
+  <div class="bg:white b:gray-20 b:1 r:12 p:6 shadow">
     <slot></slot>
   </div>
 </template>
--- packages/ui/src/components/select/Select.vue
+++ packages/ui/src/components/select/Select.vue
@@ -3,6 +3,10 @@ import Input from '../input/Input.vue'
 import IconDown from '../icon/IconChevronDown.vue'
 import Datalist from '../datalist/Datalist.vue'
 
+const id = Math.random().toString(36).substring(7)
+const idLabel = `select-label-${id}`
+const idOptions = `select-options-${id}`
+
 function showPopover(e: MouseEvent) {
   e.target?.popoverTargetElement.showPopover()
 }
@@ -11,26 +15,39 @@ function showPopover(e: MouseEvent) {
 <template>
   <div class="rotate(180):has(:popover-open)_svg">
     <Input
-      id="menu-button"
-      id-label="menu-label"
+      class="{anchor-name:--select-button}"
       label="Label"
-      popovertarget="dropdown-menu"
+      :id-label="idLabel"
+      :popovertarget="idOptions"
       @click="showPopover"
     >
       <template #right-aside>
         <IconDown class="flex-shrink:0 fg:gray-60 transition:transform|100ms|linear" width="20" />
       </template>
     </Input>
-    <Datalist id="dropdown-menu" popover anchor="menu-label">
-      popover
+    <Datalist class="bg:gray-50/.1>*:hover" popover :id="idOptions" :anchor="idLabel">
+      <option value="">ejemplo 1</option>
+      <option value="">ejemplo 2</option>
+      <option value="">ejemplo 3</option>
+      <option value="">ejemplo 4</option>
     </Datalist>
   </div>
 </template>
 
 <style scoped>
-#dropdown-menu {
+[popover] {
+  width: anchor-size(width);
   top: calc(anchor(auto) + 12px);
   bottom: auto;
   left: anchor(left);
+  margin: 0;
+}
+option {
+  padding: 8px 12px;
+  cursor: pointer;
+  transition: background-color 100ms linear;
+}
+option:hover {
+  border-radius: 8px;
 }
 </style>
