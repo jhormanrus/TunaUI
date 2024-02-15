fce94e0f087036836fec267cce39bfd150e912e7
Jhorman <jhormanrus@gmail.com>
2024-02-15 04:54:59 +0000

    🧪 test anchored indicator

--- packages/ui/src/components/select/Select.vue
+++ packages/ui/src/components/select/Select.vue
@@ -25,16 +25,17 @@ function showPopover(e: MouseEvent) {
         <IconDown class="flex-shrink:0 fg:gray-60 transition:transform|100ms|linear" width="20" />
       </template>
     </Input>
-    <Datalist class="bg:gray-50/.1>*:hover" popover :id="idOptions" :anchor="idLabel">
+    <Datalist popover :id="idOptions" :anchor="idLabel">
       <option value="">ejemplo 1</option>
       <option value="">ejemplo 2</option>
       <option value="">ejemplo 3</option>
       <option value="">ejemplo 4</option>
+      <span id="indicator" class="bg:gray-50/.1 r:8 abs transition:all|200ms|ease-in"></span>
     </Datalist>
   </div>
 </template>
 
-<style scoped>
+<style>
 [popover] {
   width: anchor-size(width);
   top: calc(anchor(auto) + 12px);
@@ -47,7 +48,23 @@ option {
   cursor: pointer;
   transition: background-color 100ms linear;
 }
-option:hover {
-  border-radius: 8px;
+option:nth-of-type(1) {
+  anchor-name: --option-hover;
+}
+option:nth-of-type(2) {
+  anchor-name: --option-hover2;
+}
+:root:has(option:nth-of-type(1):is(:hover)) {
+  --active: --option-hover;
+}
+:root:has(option:nth-of-type(2):is(:hover)) {
+  --active: --option-hover2;
+}
+#indicator {
+  top: anchor(var(--active, --option-hover) top);
+  bottom: anchor(var(--active, --option-hover) bottom);
+  left: anchor(var(--active, --option-hover) left);
+  right: anchor(var(--active, --option-hover) right);
+  z-index: -1;
 }
 </style>
